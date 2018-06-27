582c6483dc45be717c2edf17f24e49fbccb22e9a
lastlySly <lastlySly@163.com>
2018-06-27 10:43:38 +0800

    livechat实时聊天，整合完redis，logback，shiro,websocket。基本环境搭建完成。

---
+++ src/main/java/cn/lastlysly/config/websocket/WebSocketConfig.java
@@ -0,0 +1,40 @@
+package cn.lastlysly.config.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author lastlySly
+ * @GitHub https://github.com/lastlySly
+ * @create 2018-06-27 10:24
+ **/
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * 注册端点，发布或者订阅消息的时候需要连接此端点
+     * setAllowedOrigins 非必须，*表示允许其他域进行连接
+     * withSockJS  表示开启sockejs支持
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/endpoint-websocket").setAllowedOrigins("*").withSockJS();
+    }
+
+    /**
+     * 配置消息代理(中介)
+     * enableSimpleBroker 服务端推送给客户端的路径前缀
+     * setApplicationDestinationPrefixes  客户端发送数据给服务器端的一个前缀
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/chat","/mysystem");
+        registry.setApplicationDestinationPrefixes("/app");
+        // 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/
+//        registry.setUserDestinationPrefix("/user/");
+    }
+}
